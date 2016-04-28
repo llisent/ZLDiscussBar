931d56da599714b30f0ddcd00aa11dbc75be8c56
赵新 <zhaoxin@yonglibao.com>
2016-04-28 11:45:55 +0800

    no message

---
+++ ZLDisucssBar/Views/CostomPosts/ZLCostomPostsCell.h
@@ -0,0 +1,13 @@
+//
+//  ZLCostomPostsCell.h
+//  ZLDisucssBar
+//
+//  Created by Mrr on 16/4/28.
+//  Copyright © 2016年 llisent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZLCostomPostsCell : MGSwipeTableCell
+
+@end
---
+++ ZLDisucssBar/Views/CostomPosts/ZLCostomPostsCell.m
@@ -0,0 +1,24 @@
+//
+//  ZLCostomPostsCell.m
+//  ZLDisucssBar
+//
+//  Created by Mrr on 16/4/28.
+//  Copyright © 2016年 llisent. All rights reserved.
+//
+
+#import "ZLCostomPostsCell.h"
+
+@implementation ZLCostomPostsCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
